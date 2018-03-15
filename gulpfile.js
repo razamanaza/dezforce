e451b3cb0935b8ca2f95ab6582d1b4332be63123
dfkadyr <dfkadyr@gmail.com>
2018-03-15 22:56:54 +0300

    Add imagemin task, and build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
-const gulp          = require('gulp'),
-		gutil         = require('gulp-util' ),
-		sass          = require('gulp-sass'),
-		browsersync   = require('browser-sync'),
-		concat        = require('gulp-concat'),
-		uglify        = require('gulp-uglify'),
-		cleancss      = require('gulp-clean-css'),
-		rename        = require('gulp-rename'),
-		autoprefixer  = require('gulp-autoprefixer'),
-		notify        = require("gulp-notify");
+const gulp        	= require('gulp'),
+			gutil         = require('gulp-util' ),
+			sass          = require('gulp-sass'),
+			browsersync   = require('browser-sync'),
+			concat        = require('gulp-concat'),
+			uglify        = require('gulp-uglify'),
+			del         	= require('del'),
+			cleancss      = require('gulp-clean-css'),
+			rename        = require('gulp-rename'),
+			autoprefixer  = require('gulp-autoprefixer'),
+			imagemin    = require('gulp-imagemin'),
+			pngquant    = require('imagemin-pngquant'),
+			cache       = require('gulp-cache'),
+			notify        = require("gulp-notify");
 
 gulp.task('browser-sync', () => {
 	browsersync({
@@ -19,18 +23,21 @@ gulp.task('browser-sync', () => {
 });
 
 gulp.task('sass', () => {
+	return gulp.src('src/sass/**/*.scss')
+	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(gulp.dest('src/css'))
+	.pipe(browsersync.reload( {stream: true} ))
+});
+
+gulp.task('css-libs', ['sass'], function() {
 	return gulp.src([
 		'node_modules/normalize.css/normalize.css',
 		'node_modules/font-awesome/css/font-awesome.min.css',
-		'src/sass/**/*.scss'
 	])
-	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
-	//.pipe(rename({ suffix: '.min', prefix : '' }))
-	.pipe(concat('main.min.css'))
-	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(concat('libs.min.css'))
 	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
 	.pipe(gulp.dest('src/css'))
-	.pipe(browsersync.reload( {stream: true} ))
 });
 
 gulp.task('fonts', () => {
@@ -45,15 +52,46 @@ gulp.task('js', () => {
 		'src/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
+	.pipe(uglify())
 	.pipe(gulp.dest('src/js'))
 	.pipe(browsersync.reload({ stream: true }))
 });
 
-gulp.task('watch', ['fonts', 'sass', 'js', 'browser-sync'], () => {
+gulp.task('img', function() {
+	return gulp.src('src/img/**/*') 
+			.pipe(cache(imagemin({
+					interlaced: true,
+					progressive: true,
+					svgoPlugins: [{removeViewBox: false}],
+					use: [pngquant()]
+			})))
+			.pipe(gulp.dest('dist/img')); 
+});
+
+gulp.task('clean', function() {
+	return del.sync('dist');
+});
+
+gulp.task('watch', ['js', 'browser-sync', 'css-libs'], () => {
 	gulp.watch('node_modules/font-awesome/fonts/*', ['fonts']);
 	gulp.watch('src/sass/**/*.scss', ['sass']);
-	gulp.watch(['node_modules/jquery/dist/jquery.min.js', 'src/js/common.js'], ['js']);
 	gulp.watch('src/*.html', browsersync.reload)
 });
 
-gulp.task('default', ['watch']);
+
+gulp.task('build', ['clean', 'img', 'sass', 'js'], () => {
+	const buildCss = gulp.src([
+		'src/css/libs.min.css',
+		'src/css/main.css'
+	])
+		.pipe(gulp.dest('dist/css'));
+
+	const buildFonts = gulp.src('src/fonts/**/*') 
+		.pipe(gulp.dest('dist/fonts'));
+
+	const buildJs = gulp.src('src/js/scripts.min.js') 
+		.pipe(gulp.dest('dist/js'));
+
+	const buildHtml = gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+});
